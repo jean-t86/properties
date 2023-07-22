10fd50d25f5cbe3fc32cc1db0d633357aee9db77
Jean Tadebois <jpm.tadebois@gmail.com>
2023-07-22 13:03:02 +1000

    Refactor PropertyApi to use dependency injection

---
+++ app/src/main/java/me/tadebois/properties/di/PropertyApiModule.kt
@@ -0,0 +1,32 @@
+package me.tadebois.properties.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import me.tadebois.properties.propertyapi.PropertyApi
+import me.tadebois.properties.propertyapi.PropertyApiService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class PropertyApiModule {
+    @Provides
+    @Singleton
+    fun providePropertyApi(apiService: PropertyApiService): PropertyApi {
+        return PropertyApi(apiService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNedApiService(): PropertyApiService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://f213b61d-6411-4018-a178-53863ed9f8ec.mock.pstmn.io/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        return retrofit.create(PropertyApiService::class.java)
+    }
+}
--- app/src/main/java/me/tadebois/properties/propertyapi/PropertyApi.kt
+++ app/src/main/java/me/tadebois/properties/propertyapi/PropertyApi.kt
@@ -1,18 +1,6 @@
 package me.tadebois.properties.propertyapi
 
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
-class PropertyApi {
-    private val apiService: PropertyApiService by lazy {
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://f213b61d-6411-4018-a178-53863ed9f8ec.mock.pstmn.io")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        retrofit.create(PropertyApiService::class.java)
-    }
-
+class PropertyApi(private val apiService: PropertyApiService) {
     suspend fun getProperties(): ApiResponse {
         return apiService.getProperties()
     }
