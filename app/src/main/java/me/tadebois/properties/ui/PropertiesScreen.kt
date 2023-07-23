5713a762efee66d942eca017e4d7d7495ea8b97b
Jean Tadebois <jpm.tadebois@gmail.com>
2023-07-23 13:35:04 +1000

    Add LazyRow for property images
    At the moment, all images are loaded and cached before displaying. This isn't great UI/UX. In a coming commit, I load images asynchronously and cache them. A placeholder image will appear while the image is being loaded.

--- app/src/main/java/me/tadebois/properties/ui/PropertiesScreen.kt
+++ app/src/main/java/me/tadebois/properties/ui/PropertiesScreen.kt
@@ -1,10 +1,16 @@
 package me.tadebois.properties.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -15,14 +21,25 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import me.tadebois.properties.R
 import me.tadebois.properties.api.Property
+import me.tadebois.properties.api.PropertyImage
 import me.tadebois.properties.ui.Helpers.getProperty
 import me.tadebois.properties.ui.theme.PropertiesTheme
 
@@ -61,8 +78,11 @@ fun PropertiesScreen(propertyViewModel: PropertyViewModel = viewModel()) {
 }
 
 @Composable
-fun PropertyList(properties: List<Property>) {
-    Column {
+fun PropertyList(
+    properties: List<Property>,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
         properties.forEach { property ->
             PropertyItem(property)
         }
@@ -83,18 +103,64 @@ fun PropertyItem(
     modifier: Modifier = Modifier
 ) {
     Column(modifier = modifier.padding(16.dp)) {
-        Text(text = "Property ID: ${property.id}")
-        Spacer(modifier = modifier.height(4.dp))
-        Text(text = "Description: ${property.description}")
+        ImageCarousel(propertyImages = property.propertyImages)
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun PropertyItemPreview() {
     PropertiesTheme {
         PropertyItem(
             getProperty()
         )
+    }
+}
+
+@Composable
+fun ImageCarousel(
+    propertyImages: List<PropertyImage>,
+    modifier: Modifier = Modifier
+) {
+    val imageUrls = propertyImages.map { it.attachment.medium.url }
+    val context = LocalContext.current
+
+    // Load and cache all the images only once, after the composable is committed
+    val imageBitmaps by rememberUpdatedState(newValue = runBlocking {
+        imageUrls.map { imageUrl ->
+            val requestOptions = RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.DATA) // Cache the original image size
+
+            withContext(Dispatchers.IO) {
+                Glide.with(context)
+                    .asBitmap()
+                    .load(imageUrl)
+                    .apply(requestOptions)
+                    .submit() // Load the image synchronously (non-blocking)
+                    .get() // Get the result (blocking)
+                    .asImageBitmap() // Convert to ImageBitmap
+            }
+        }
+    })
+
+    LazyRow(
+        modifier = modifier
+            .fillMaxWidth()
+            .aspectRatio(16 / 9f)
+    ) {
+        itemsIndexed(imageBitmaps) { _, imageBitmap ->
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(16 / 9f)
+            ) {
+                Image(
+                    bitmap = imageBitmap,
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
     }
 }
